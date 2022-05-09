79c738778e43e0a2bcb4e09883fdb94a744b49b1
Javier Cano De La Torre <xhiibrids2@gmail.com>
2022-05-09 12:13:05 +0200

    alembic sin migraciones

--- server/models.py
+++ server/models.py
@@ -13,11 +13,15 @@ class Cliente(Base):
     __tablename__ = "clientes"
 
     id = Column(Integer, primary_key=True, index=True)
+    vehiculos = relationship("Vehiculo",back_populates="owner")
 class Vehiculo(Base):
     __tablename__ = "vehiculos"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("clientes.id"))
+    cliente = Column(Integer, ForeignKey("clientes.id"))
+    owner =     relationship("Cliente",back_populates="vehiculos")
+class Trabajo(Base):
+    __tablename__ = "trabajos"
+    id = Column(Integer,primary_key =True,index=True)
+    cliente_id = Column(Integer,ForeignKey("clientes.id"))
 
