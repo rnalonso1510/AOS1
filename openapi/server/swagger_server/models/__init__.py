936b52ac88437c7e17600c2ab895eff2c02a3c84
Javier Cano De La Torre <j.cdelatorre@alumnos.upm.es>
2022-05-18 21:28:23 +0200

    Eliminados ficheros autogerados por codegen

--- openapi/server/swagger_server/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from swagger_server.models.factura import Factura
-from swagger_server.models.factura_request_body import FacturaRequestBody
-from swagger_server.models.http_problem import HTTPProblem
-from swagger_server.models.trabajo import Trabajo
